7559f6ea2a446ae0d8c8d14174f9ef892f727b18
Abhishek Nair <abhisheknr4@gmail.com>
2023-08-17 18:59:33 +0000

    Pending changes exported from your codespace

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  
+}
